c3024296b3dfd73725d7475b7014bd41a9f4ad41
moremirim <moremirim@gmail.com>
2024-05-22 21:23:03 +0900

    Completed Flash Chat app

--- messaging-app:Firestore/Flash Chat iOS13/Controllers/LoginViewController.swift
+++ messaging-app:Firestore/Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -7,6 +7,10 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
+      
 
 class LoginViewController: UIViewController {
 
@@ -15,6 +19,16 @@ class LoginViewController: UIViewController {
     
 
     @IBAction func loginPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e.localizedDescription)
+                } else {
+                    self.performSegue(withIdentifier: K.loginSegue, sender: self)
+                    //sender = startinf point of this segue
+                }
+            }
+        }
     }
     
 }
--- messaging-app:Firestore/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ messaging-app:Firestore/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -12,17 +12,27 @@ class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         titleLabel.text = ""
         var charIndex = 0.0
-        let titleText = "⚡️FlashChat"
+        let titleText = K.appName
         for letter in titleText {
-            print("-")
-            print(charIndex)
-            print(letter)
-            Timer(timeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+//            print("-")
+//            print(charIndex)
+//            print(letter)
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
                 self.titleLabel.text?.append(letter)
             }
             charIndex += 1
